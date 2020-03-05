b300f5dec28bd3a871368a8c8259c44eca312149
afonso_rosa@hotmail.com <afonso_rosa@hotmail.com>
2020-03-05 10:33:59 +0000

    Cilindro complete

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,7 +6,31 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Variables
+            string height;
+            float h;
+            string radius;
+            float r;
+            float v;
+            float sa;
+
+            // Ask for user inputs
+            Console.WriteLine("What's the height of the cylinder?");
+            height = Console.ReadLine();
+            Console.WriteLine("What's the radius of the cylinder?");
+            radius = Console.ReadLine();   
+
+            // Convert floats into strings
+            h = float.Parse(height);
+            r = float.Parse(radius);
+
+            // Calculating volume and surface area
+            v = 3.1415926f*(r*r)*h;
+            sa = 2 * 3.1415926f * r * (r + h); 
+
+            // Presenting results
+            Console.WriteLine("The volume of the cylinder is " + v); 
+            Console.WriteLine("The surface area of the cylinder is " + sa); 
         }
     }
 }
