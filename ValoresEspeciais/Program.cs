6d8ac5426639adbd693d5bdd0c1376f45075cd8d
afonso_rosa@hotmail.com <afonso_rosa@hotmail.com>
2020-03-05 11:48:53 +0000

    Ex07 complete

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -76,6 +76,21 @@ namespace ValoresEspeciais
             Console.WriteLine(fn);
             Console.WriteLine(x);
             Console.WriteLine(y);
+
+            // Variables Phase 03
+            int i = int.MaxValue;
+            double z = 2 * double.MaxValue;
+            double z1 = double.MaxValue + 1;
+            float f1, f2;
+            f1 = f2 = 10000.0f;
+
+            // Display results with overflow Phase 03
+            Console.WriteLine((int) + (i + 1));
+            Console.WriteLine(z);
+            Console.WriteLine(z1);
+
+            // Display results with underflow Phase 03
+            Console.WriteLine(f1 == f2 + 0.0001f);
         }
     }
 }
