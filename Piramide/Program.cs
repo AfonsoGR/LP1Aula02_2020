e21ff13ddf59cf88ce9ea5349d3c870afc7bc7af
afonso_rosa@hotmail.com <afonso_rosa@hotmail.com>
2020-03-05 12:39:28 +0000

    Ex09 completed

--- Piramide/Program.cs
+++ Piramide/Program.cs
@@ -15,7 +15,30 @@ namespace Piramide
             slines = Console.ReadLine();
             nlines = Convert.ToInt32(slines);
 
-            // For cycle
+            // For cycle, to build the pyramid
+            for (int i = 0; i < nlines; i++)
+            {
+                // Variables for the cycle and respective calculus
+                int spaces;
+                int asterisks;
+                spaces = nlines - i - 1;
+                asterisks = i * 2 + 1;
+                
+                // Print spaces
+                for (int j = 0; j < spaces; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // Print asterisks
+                for (int j = 0; j < asterisks; j++)
+                {
+                    Console.Write("*");
+                }
+
+                // Make a new line
+                Console.WriteLine();
+            }
         }
     }
 }
