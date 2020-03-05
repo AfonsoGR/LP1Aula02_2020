871828198f7184b4ecb0b5b43a1569a86cd5f595
afonso_rosa@hotmail.com <afonso_rosa@hotmail.com>
2020-03-05 11:28:07 +0000

    Ex6 Step1

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,55 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Variables
+            sbyte sbmax = sbyte.MaxValue;
+            sbyte sbmin = sbyte.MinValue;
+
+            int imax = int.MaxValue;
+            int imin = int.MinValue;
+
+            uint uimax = uint.MaxValue;
+            uint uimin = uint.MinValue;
+
+            double dmax = double.MaxValue;
+            double dmin = double.MinValue;
+
+            long lmax = long.MaxValue;
+            long lmin = long.MinValue;
+
+            ulong ulmax = ulong.MaxValue;
+            ulong ulmin = ulong.MinValue;
+
+            float fmax = float.MaxValue;
+            float fmin = float.MinValue;
+
+            short smax = short.MaxValue;
+            short smin = short.MinValue;
+
+            // Print values
+            Console.WriteLine(sbmax);
+            Console.WriteLine(sbmin);
+            
+            Console.WriteLine(imax);
+            Console.WriteLine(imin);
+            
+            Console.WriteLine(uimax);
+            Console.WriteLine(uimin);
+            
+            Console.WriteLine(dmax);
+            Console.WriteLine(dmin);
+            
+            Console.WriteLine(lmax);
+            Console.WriteLine(lmin);
+            
+            Console.WriteLine(ulmax);
+            Console.WriteLine(ulmin);
+
+            Console.WriteLine(fmax);
+            Console.WriteLine(fmin);
+
+            Console.WriteLine(smax);
+            Console.WriteLine(smin);
         }
     }
 }
